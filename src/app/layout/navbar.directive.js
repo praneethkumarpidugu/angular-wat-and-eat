7d429313a9aa7b92d24f9a12a690ae64cfde2d11
praneethkumarpidugu <pidugu2007@gmail.com>
2016-06-16 19:44:19 -0400

    template for navbar working

---
+++ src/app/layout/navbar.directive.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+	
+	angular
+		.module('app.layout')
+		.directive('gzNavbar', gzNavbar);
+	function gzNavbar() {
+		return {
+			templateUrl:'app/layout/navbar.html',
+			restrict: 'E'
+		};
+	}
+	
+})();
