86858eb9f075a209256fc51aae231aab945a8867
praneethkumarpidugu <pidugu2007@gmail.com>
2016-06-16 15:23:38 -0400

    Used TextMessageService.sendTextMessage in WaitlistController

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -5,9 +5,9 @@
 		.module('app.waitList')
 		.controller('WaitListController', WaitListController);
 	
-	WaitListController.$inject = ['firebaseDataService', 'partyService'];
+	WaitListController.$inject = ['partyService', 'textMessageService'];
 	
-	function WaitListController(firebaseDataService, partyService) {
+	function WaitListController(partyService, textMessageService) {
 		var vm = this;
 		
 		var fireParties = new Firebase(FIREBASE_URL + 'parties');
@@ -38,18 +38,7 @@
 		
 		//function to sendtext message
 		function sendTextMessage(party) {
-			var newTextMessage = {
-				phoneNumber: party.phone,
-				size: party.size,
-				name: party.name
-			};
-			//send the above newTextMessage with firebaseDataService
-			firebaseDataService.textMessages.push(newTextMessage);
-			//we will notify the other side.
-			party.notified = true;
-			
-			//save the database  i,e vm.parties with $save directive.
-			vm.parties.$save(party)
+			textMessageService.sendTextMessage(party, vm.parties);
 		}
 		
 		
