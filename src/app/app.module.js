afd7939ca62214cdbe969fced87b7ebd8d313af9
praneethkumarpidugu <pidugu2007@gmail.com>
2016-06-17 00:56:21 -0400

    Added  listener that enforces auth required

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -16,11 +16,23 @@
 					'app.waitList'
 		])
 		.config(configFunction);
+		.run(runFunction);
+	
 	configFunction.$inject = ['$routeProvider'];
 	
 	function configFunction($routeProvider) {
 		$routeProvider.otherwise({
 			redirectTo: '/'
+		});
+	}
+	
+	runFunction.$inject = ['$rootScope', '$location'];
+	
+	function runFunction($rootScope, $location) {
+		$rootScope.$on('$routeChangeError', function(event, next, previous, error){
+			if (error === "AUTH_REQUIRED") {
+				$location.path('/');
+			}
 		});
 	}
 })();
