90df53b83af626c87d313fbd45861e47880cc355
praneethkumarpidugu <pidugu2007@gmail.com>
2016-06-17 12:51:06 -0400

    Setup for gzPartForm directive

---
+++ src/app/waitList/directives/partyForm.directive.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+	
+	angular
+		.module('app.waitList')
+		.directive('gzPartyForm', gzPartyForm);
+	
+	function gzPartyForm() {
+		return {
+			templateUrl: 'app/waitlist/directives/partyForm.html',
+			restrict: 'E'
+		};
+	}
+})();
