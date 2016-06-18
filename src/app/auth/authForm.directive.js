8c4f97e828bcd9904b68aa5f2eaca23748246213
praneethkumarpidugu <pidugu2007@gmail.com>
2016-06-18 13:16:41 -0400

    Added initial setup for gzAuthForm

---
+++ src/app/auth/authForm.directive.js
@@ -0,0 +1,23 @@
+(function() {
+	'use strict';
+	
+	angular
+		.module('app.auth')
+		.directive('gzAuthForm', gzAuthForm);
+	
+	function gzAuthForm() {
+		return {
+			templateUrl: 'app/auth/authForm.html',
+			restrict: 'E',
+			controller: AuthFormController,
+			controllerAs: 'vm',
+			bindToController: true,
+			scope: {}
+		};
+	}
+	
+	function AuthFormController() {
+		var vm = this;
+	}
+	
+})();
